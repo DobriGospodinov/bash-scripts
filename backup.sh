2963f11536e86bb59ef88bd12167334312d7a054
Dobri.Gospodinov <28982299+DobriGospodinov@users.noreply.github.com>
2024-04-12 20:18:13 +0300

    added functionality to check if the specified directories exist

--- backup.sh
+++ backup.sh
@@ -10,8 +10,23 @@
 #################################################################################
 
 echo "Hello, ${USER^}"
+
 read -p "Please enter the path of the directory you want to back up: " backup_dir
+
+# check if directory that will be backed up exists:
+if [ ! -d "$backup_dir" ]; then
+    echo "Error: The specified backup directory does not exist or is not a directory."
+    exit 1
+fi
+
 read -p "Please enter the path of the directory where you want to store the backup: " output_dir
+
+# check if directory where the backup will be stored exists:
+if [ ! -d "$backup_dir" ]; then
+    echo "Error: The specified backup directory does not exist or is not a directory."
+    exit 1
+fi
+
 tar zcf "$output_dir"/my_backup_"$(date +%d-%m-%Y_%H-%M-%S)".tar.gz "$backup_dir"/* 2>/dev/null
 echo "Backup Completed Successfully."
 exit 0 
